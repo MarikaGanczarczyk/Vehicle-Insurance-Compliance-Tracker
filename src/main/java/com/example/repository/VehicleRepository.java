fc929eb9354f874d9033c904fe5f115e6a177b27
chris1504896 <sunny_9194@hotmail.com>
2026-05-20 14:36:01 +0100

    created vehicle entity

---
+++ src/main/java/com/example/repository/VehicleRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+
+
+
+}
