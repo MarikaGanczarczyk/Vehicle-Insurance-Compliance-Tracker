3752bae6e45402f2e1d1a236ed6cf9ef23adf59d
marik1504894 <marik1504894@Mastek.com>
2026-05-20 14:20:47 +0100

    Added  Insurance Policy entity

---
+++ src/main/java/com/example/service/InsurancePolicyService.java
@@ -0,0 +1,27 @@
+package com.example.service;
+
+import com.example.entity.InsurancePolicy;
+import com.example.repository.InsurancePolicyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InsurancePolicyService {
+
+
+    @Autowired
+    private InsurancePolicyRepository repo;
+
+    public InsurancePolicy addPolicy(InsurancePolicy policy) {
+        return repo.save(policy);
+    }
+
+    public InsurancePolicy getPolicyByVehicle(Integer vehicleId) {
+        return repo.findAll()
+                .stream()
+                .filter(p -> p.getVehicle().getId().equals(vehicleId))
+                .findFirst()
+                .orElse(null);
+    }
+
+}
