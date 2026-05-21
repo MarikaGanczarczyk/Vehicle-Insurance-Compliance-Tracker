c4fd2444c8e2960ca8660e5e3c784df4747259bf
chris1504896 <sunny_9194@hotmail.com>
2026-05-21 14:28:50 +0100

    get/put

--- src/main/java/com/example/controller/VehicleController.java
+++ src/main/java/com/example/controller/VehicleController.java
@@ -3,14 +3,14 @@ package com.example.controller;
 import com.example.entity.Vehicle;
 import com.example.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/vehicles")
 public class VehicleController {
-
 
     @Autowired
     private VehicleService service;
@@ -26,8 +26,23 @@ public class VehicleController {
 
     }
 
-    //We need @GetMapping by vehicleId
-    //We need @PutMapping by vehicleId
+    //GET /api/vehicles/{ID}
+    @GetMapping("/{id}")
+    public ResponseEntity<Vehicle> getVehicleById(@PathVariable int id) {
+
+        Vehicle vehicle = service.getVehicleById(id);
+        return ResponseEntity.ok(vehicle);
+    }
+
+    //PUT /api/vehicles/{ID}
+    @PutMapping("/{id}")
+    public ResponseEntity<Vehicle> updateVehicle(
+            @PathVariable int id,
+            @RequestBody Vehicle updatedVehicle) {
+
+        Vehicle vehicle = service.updateVehicleById(id, updatedVehicle);
+        return ResponseEntity.ok(vehicle);
+    }
 
     @DeleteMapping("/vehicles/{id}")
     public void deleteVehicle(@PathVariable Integer id) {
