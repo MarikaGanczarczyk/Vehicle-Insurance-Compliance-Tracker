809763b6e191687a733fe7926f0e0ee7d70bb49d
marik1504894 <marik1504894@Mastek.com>
2026-05-27 15:57:52 +0100

    Created csvReportController & csvReportService and ComplianceController

--- src/main/java/com/example/repository/VehicleViolationRepository.java
+++ src/main/java/com/example/repository/VehicleViolationRepository.java
@@ -3,8 +3,10 @@ package com.example.repository;
 import com.example.entity.VehicleViolation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,4 +19,7 @@ public interface VehicleViolationRepository extends JpaRepository<VehicleViolati
             order by violation.dateTime desc
             """)
     List<VehicleViolation> findAllLatestFirst();
+
+    @Query("SELECT v FROM VehicleViolation v WHERE CAST(v.dateTime AS date) = :date")
+    List<VehicleViolation> findByDate(@Param("date") LocalDate date);
 }
