ee89403c5eb1327e799db1bec0f4ed0498338fb5
marik1504894 <marik1504894@Mastek.com>
2026-05-27 15:57:52 +0100

    Created csvReportController & csvReportService and ComplianceController

---
+++ src/main/java/com/example/controller/ComplianceController.java
@@ -0,0 +1,35 @@
+package com.example.controller;
+
+import com.example.entity.VehicleViolation;
+import com.example.repository.VehicleViolationRepository;
+import com.example.service.ComplianceService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/compliance")
+public class ComplianceController {
+
+
+    private  ComplianceService complianceService;
+    private VehicleViolationRepository vehicleViolationRepository;
+
+    public ComplianceController(ComplianceService complianceService, VehicleViolationRepository vehicleViolationRepository) {
+        this.complianceService = complianceService;
+        this.vehicleViolationRepository = vehicleViolationRepository;
+    }
+
+
+    @PostMapping("/run")
+    public List<VehicleViolation> runCompliance() {
+        return complianceService.runComplianceCheck();
+    }
+    @GetMapping("/violations")
+    public List<VehicleViolation> getAllViolations() {
+        return vehicleViolationRepository.findAll();
+    }
+}
