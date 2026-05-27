52a354a95c83082bbacee88c2ab78fc99d9f50e5
erikb <erikbajraktari878@gmail.com>
2026-05-27 13:36:17 +0100

    error fix

--- src/main/java/com/example/repository/VehicleTripsRepository.java
+++ src/main/java/com/example/repository/VehicleTripsRepository.java
@@ -2,8 +2,18 @@ package com.example.repository;
 
 import com.example.entity.VehicleTrips;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VehicleTripsRepository extends JpaRepository<VehicleTrips, Integer> {
+    @Query("""
+            select trip from VehicleTrips trip
+            where trip.vehicleId = :vehicleId
+            order by trip.dateTime asc
+            """)
+    List<VehicleTrips> findTripHistoryByVehicleId(@Param("vehicleId") int vehicleId);
 }
