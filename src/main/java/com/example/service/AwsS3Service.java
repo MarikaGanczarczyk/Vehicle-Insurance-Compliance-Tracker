b002b1f84106893f4ee47d9a68d8a2fe4cad3077
marik1504894 <marik1504894@Mastek.com>
2026-05-28 15:13:21 +0100

    Created Aws and s3 service & new method in csvreport

---
+++ src/main/java/com/example/service/AwsS3Service.java
@@ -0,0 +1,34 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+
+@Service
+public class AwsS3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket-name}")
+    private String bucketName;
+
+    public AwsS3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public String uploadFile(File file) {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(file.getName())        // nazwa pliku w S3
+                .contentType("text/csv")
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromFile(file));
+
+        return "https://" + bucketName + ".s3.amazonaws.com/" + file.getName();
+    }
+}
